1002c4facb52db8c756b0c8d90824815ba7abbcc
y4ngyy <591620892@qq.com>
2023-11-15 11:00:32 +0800

    fix compile error

--- symsan_configure.sh
+++ symsan_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf build
-CXXFLAGS="-stdlib=libc++ -pthread" CC=clang-12 CXX=clang++-12 ./configure.py \
+CXXFLAGS="-stdlib=libc++ -lc++abi -pthread" CC=clang-12 CXX=clang++-12 ./configure.py \
     --static --no-testing --no-unit-testing
